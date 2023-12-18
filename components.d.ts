0166e2b8a2e0d93413ce18626a354b3f0a9a206c
wangchao <wangchao2423@gmail.com>
2023-12-18 11:28:54 +0800

    feat: 测试 .config 文件夹

--- components.d.ts
+++ components.d.ts
@@ -13,7 +13,6 @@ declare module 'vue' {
     ElCollapse: typeof import('element-plus/es')['ElCollapse']
     ElCollapseItem: typeof import('element-plus/es')['ElCollapseItem']
     ElDialog: typeof import('element-plus/es')['ElDialog']
-    ElDivider: typeof import('element-plus/es')['ElDivider']
     ElDrawer: typeof import('element-plus/es')['ElDrawer']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
